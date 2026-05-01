3fed3499a942f76b7fb3353f59562ff5d50b29b4
anika-dewari <anikadewari26@gmail.com>
2026-05-01 22:30:04 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-B_qikw1N.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/anika/Desktop/portfolio/interactive-portfolio-hub/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-KDzU4V9h.js"] }, "/": { filePath: "C:/Users/anika/Desktop/portfolio/interactive-portfolio-hub/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BmhzZYrp.js"] } }, clientEntry: "/assets/index-KDzU4V9h.js" });
+export {
+  tsrStartManifest
+};
